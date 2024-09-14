6898888b9ecf2658241af945fe2d649242986d19
Ruslan Rodionov <ruslan.rodionow98@gmail.com>
2024-09-15 01:01:19 +0530

    feat: add loggs mw

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,5 +1,32 @@
 import { Message } from "node-telegram-bot-api";
+import { User } from "../database/users";
+import { Logs } from "../database/loggs";
 
-export const withMessageLogger = async (msg: Message) => {
-  return msg;
-};
+export function logFunction(cb: (msg: Message) => any) {
+  return async function (data: Message) {
+    try {
+      const chatId = data.chat?.id;
+      let user = await User.findOne({ chat_id: chatId });
+      if (!user) {
+        user = await User.create({
+          chat_id: chatId,
+          first_name: data.chat.first_name,
+          last_name: data.chat.last_name,
+          username: data.sender_chat?.active_usernames,
+          language_code: data.from?.language_code,
+        });
+      }
+
+      await Logs.create({
+        user_id: user._id,
+        message_id: data.message_id,
+        text: data.text || "",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    const result = await cb(data);
+    return result;
+  };
+}
--- src/services/bot.ts
+++ src/services/bot.ts
@@ -1,15 +1,14 @@
 import fs from "fs";
 import TgBot from "node-telegram-bot-api";
 import { YtDownload } from "./ytdl";
-import { User } from "../database/users";
-import { Logs } from "../database/loggs";
+import { logFunction } from "../helpers/logger";
 
 const ytRegex =
   /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:watch\?v=|embed\/|v\/|.+\/.+\/|user\/\w+\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/g;
 
 export class TelegramBot {
-  bot: TgBot;
-  ytdl: YtDownload;
+  private bot: TgBot;
+  private ytdl: YtDownload;
 
   constructor() {
     this.bot = new TgBot(process.env.TELEGRAM_BOT_KEY || "", {
@@ -19,67 +18,55 @@ export class TelegramBot {
   }
 
   async init() {
-    this.bot.onText(/\/start/, async (msg) => {
-      const chatId = msg.chat.id;
-      try {
-        let user = await User.findOne({ chat_id: chatId });
-        if (!user) {
-          user = await User.create({
-            chat_id: chatId,
-            first_name: msg.chat.first_name,
-            last_name: msg.chat.last_name,
-            username: msg.sender_chat?.active_usernames,
-            language_code: msg.from?.language_code,
-          });
-        }
-
+    this.bot.onText(
+      /\/start/,
+      logFunction(async (msg) => {
+        const chatId = msg.chat.id;
         await this.bot.sendMessage(chatId, "Hi i'm bot");
-        await Logs.create({
-          user_id: user._id,
-          message_id: msg.message_id,
-          text: "/start",
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    });
+      })
+    );
   }
 
   async ytLinkListen() {
-    this.bot.onText(ytRegex, async (msg) => {
-      const chatId = msg.chat.id;
-      const userText = msg.text;
+    this.bot.onText(
+      ytRegex,
+      logFunction(async (msg) => {
+        const chatId = msg.chat.id;
+        const userText = msg.text;
 
-      const isValidLink = this.ytdl.validateLink(userText || "");
+        const isValidLink = this.ytdl.validateLink(userText || "");
 
-      if (isValidLink && userText) {
-        await this.ytdl.getAudio({
-          chatId,
-          videoURL: userText,
-          onSuccess: async ({ path, name }) => {
-            const file = fs.readFileSync(path);
-            await this.bot.sendAudio(chatId, file, {}, { filename: name });
-            setTimeout(() => {
-              console.log("delete");
+        if (isValidLink && userText) {
+          await this.ytdl.getAudio({
+            chatId,
+            videoURL: userText,
+            onSuccess: async ({ path, name }) => {
+              const file = fs.readFileSync(path);
+              await this.bot.sendAudio(chatId, file, {}, { filename: name });
+              setTimeout(() => {
+                this.deleteFile(path);
+              }, 30 * 1000);
+            },
+            onError: async ({ message, path }) => {
+              await this.bot.sendMessage(chatId, `Error: ${message}`);
               this.deleteFile(path);
-            }, 60 * 1000);
-          },
-          onError: async ({ message, path }) => {
-            await this.bot.sendMessage(chatId, `Error: ${message}`);
-            this.deleteFile(path);
-          },
-        });
-      } else {
-        await this.bot.sendMessage(chatId, "Invalid link");
-      }
-    });
+            },
+          });
+        } else {
+          await this.bot.sendMessage(chatId, "Invalid link");
+        }
+      })
+    );
   }
 
   async listenMessages() {
-    this.bot.on("message", async (msg) => {
-      const chatId = msg.chat.id;
-      await this.bot.sendMessage(chatId, "Send me youtube link");
-    });
+    this.bot.on(
+      "message",
+      logFunction(async (msg) => {
+        const chatId = msg.chat.id;
+        await this.bot.sendMessage(chatId, "Send me youtube link");
+      })
+    );
   }
 
   deleteFile(path: string) {
